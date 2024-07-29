6575accd437fb8fabdaaf7930f702a9c403ce7d2
Navarre <jpnavarre@Navarres-iMac.local>
2024-07-29 15:42:57 -0400

    uploaded schema to file

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,32 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+CREATE TABLE category (
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    category_name CHAR LENGTH NOT NULL
+    
+);
+
+CREATE TABLE product(
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_name
+    price DECIMAL NOT NULL 
+    stock INTEGER NOT NULL (10) ISNUMERIC,
+    category_id INTEGER REFERENCES category(id)
+);
+
+CREATE TABLE tag(
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    tag_name INTEGER,
+);
+
+
+CREATE TABLE product_tag(
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_tag INTEGER product(id),
+    tag_id INTEGER product(id),
+);
+
+
+\c ecommerce_db;
+
